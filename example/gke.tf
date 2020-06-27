6ca729af474117cc7fcf4262d84962adf5f42c65
Gus Narea <gnarea@users.noreply.github.com>
2020-06-27 18:16:24 +0100

    Enable GCP logging so GKE pod logs can be sent there

--- example/gke.tf
+++ example/gke.tf
@@ -39,3 +39,9 @@ resource "google_container_node_pool" "primary" {
     ]
   }
 }
+
+resource "google_project_service" "logging" {
+  project = var.gcp_project_id
+  service = "logging.googleapis.com"
+  disable_dependent_services = true
+}
