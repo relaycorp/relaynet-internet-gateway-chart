5d8a56de98c00555f3246a093dc3d967449eaa9b
Gus Narea <gnarea@users.noreply.github.com>
2020-06-27 18:30:21 +0100

    Fix formatting

--- example/gke.tf
+++ example/gke.tf
@@ -41,7 +41,7 @@ resource "google_container_node_pool" "primary" {
 }
 
 resource "google_project_service" "logging" {
-  project = var.gcp_project_id
-  service = "logging.googleapis.com"
+  project                    = var.gcp_project_id
+  service                    = "logging.googleapis.com"
   disable_dependent_services = true
 }
