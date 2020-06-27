59058d09080f32745c7558fe8c41317692f0da5c
Gus Narea <gnarea@users.noreply.github.com>
2020-06-27 15:25:10 +0100

    Fix formatting

--- example/gcp.tf
+++ example/gcp.tf
@@ -1,12 +1,12 @@
 provider "google" {
   project = var.gcp_project_id
-  region = "europe-west2"
-  zone = "europe-west2-a"
+  region  = "europe-west2"
+  zone    = "europe-west2-a"
 
   version = "~> 3.27"
 }
 
 resource "google_project" "main" {
-  name = var.gcp_project_id
+  name       = var.gcp_project_id
   project_id = var.gcp_project_id
 }
