6054a1375ea4623510e23bff29c952769ca96f8c
Gus Narea <gnarea@users.noreply.github.com>
2020-09-16 23:08:26 +0100

    Allow health check probes
    As workaround for https://github.com/kubernetes/ingress-gce/issues/18#issuecomment-658765449

--- example/networking.tf
+++ example/networking.tf
@@ -11,3 +11,16 @@ resource "google_compute_network_peering" "mongodb_atlas" {
   network      = data.google_compute_network.main.self_link
   peer_network = "https://www.googleapis.com/compute/v1/projects/${mongodbatlas_network_peering.main.atlas_gcp_project_id}/global/networks/${mongodbatlas_network_peering.main.atlas_vpc_name}"
 }
+
+resource "google_compute_firewall" "neg_backend_workaround" {
+  // Workaround for https://github.com/kubernetes/ingress-gce/issues/18#issuecomment-658765449
+  name    = "neg-backend-workaround"
+  network = data.google_compute_network.main.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8081"]
+  }
+
+  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
+}
